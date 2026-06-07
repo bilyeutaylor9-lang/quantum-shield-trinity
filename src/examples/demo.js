60bc1c1901121d80769e81ace23f68ccc1189512
bilyeutaylor9-lang <bilyeutaylor9@gmail.com>
2026-06-07 09:46:36 -0700

    Refactor quantumShieldTrinity call for better readability
    Add crypto inventory demo

--- src/examples/demo.js
+++ src/examples/demo.js
@@ -1,10 +1,17 @@
 import { quantumShieldTrinity } from "../index.js";
 
-const report = quantumShieldTrinity({
-  address: "0x123456789",
-  transactionCount: 22,
-  reusedAddress: true,
-  signedMessages: 5
-});
+const report = quantumShieldTrinity(
+  {
+    address: "0x123456789",
+    transactionCount: 22,
+    reusedAddress: true,
+    signedMessages: 5
+  },
+  `
+    RSA
+    ECDSA
+    SHA1
+  `
+);
 
 console.log(JSON.stringify(report, null, 2));
