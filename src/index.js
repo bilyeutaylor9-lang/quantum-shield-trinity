1576018440391c71febb7cbd7485361d15e49ba4
bilyeutaylor9-lang <bilyeutaylor9@gmail.com>
2026-06-07 09:38:10 -0700

    Add quantumShieldTrinity function to index.js
    Connect Trinity Engines

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 export { walletRiskEngine } from "./engines/walletRiskEngine.js";
 export { cryptoInventoryEngine } from "./engines/cryptoInventoryEngine.js";
 export { migrationShieldEngine } from "./engines/migrationShieldEngine.js";
+
+export function quantumShieldTrinity(wallet) {
+  const walletReport = walletRiskEngine(wallet);
+  const inventoryReport = cryptoInventoryEngine();
+  const migrationReport = migrationShieldEngine();
+
+  return {
+    platform: "Quantum Shield Trinity",
+    version: "0.1.0",
+    walletReport,
+    inventoryReport,
+    migrationReport
+  };
+}
