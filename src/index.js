06ef227f0a986878f285bbf5b7a3a52d42da1f4e
bilyeutaylor9-lang <bilyeutaylor9@gmail.com>
2026-06-07 09:51:04 -0700

    Integrate security assessment engine and update version
    Added security assessment engine and updated version.Connect security assessment engine

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
 export { walletRiskEngine } from "./engines/walletRiskEngine.js";
 export { cryptoInventoryEngine } from "./engines/cryptoInventoryEngine.js";
 export { migrationShieldEngine } from "./engines/migrationShieldEngine.js";
+export { securityAssessmentEngine } from "./engines/securityAssessmentEngine.js";
 
 import { walletRiskEngine } from "./engines/walletRiskEngine.js";
 import { cryptoInventoryEngine } from "./engines/cryptoInventoryEngine.js";
 import { migrationShieldEngine } from "./engines/migrationShieldEngine.js";
+import { securityAssessmentEngine } from "./engines/securityAssessmentEngine.js";
 
 export function quantumShieldTrinity(wallet, codeSample = "") {
   const walletReport = walletRiskEngine(wallet);
 
-  const inventoryReport =
-    cryptoInventoryEngine(codeSample);
+  const inventoryReport = cryptoInventoryEngine(codeSample);
 
-  const migrationReport =
-    migrationShieldEngine(
-      walletReport,
-      inventoryReport
-    );
-
-  const totalScore = Math.min(
-    100,
-    walletReport.score +
-      inventoryReport.score
+  const migrationReport = migrationShieldEngine(
+    walletReport,
+    inventoryReport
   );
+
+  const assessmentReport = securityAssessmentEngine({
+    walletReport,
+    inventoryReport,
+    migrationReport
+  });
 
   return {
     platform: "Quantum Shield Trinity",
-    version: "0.3.0",
-    totalScore,
+    version: "0.4.0",
+    assessmentReport,
     walletReport,
     inventoryReport,
     migrationReport
