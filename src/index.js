85b183ce67b8d70c8fa80954e44a84d9fd9232de
bilyeutaylor9-lang <bilyeutaylor9@gmail.com>
2026-06-07 09:46:18 -0700

    Refactor quantumShieldTrinity to accept codeSample
    Add combined Trinity scoring

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ export { walletRiskEngine } from "./engines/walletRiskEngine.js";
 export { cryptoInventoryEngine } from "./engines/cryptoInventoryEngine.js";
 export { migrationShieldEngine } from "./engines/migrationShieldEngine.js";
 
-export function quantumShieldTrinity(wallet) {
+import { walletRiskEngine } from "./engines/walletRiskEngine.js";
+import { cryptoInventoryEngine } from "./engines/cryptoInventoryEngine.js";
+import { migrationShieldEngine } from "./engines/migrationShieldEngine.js";
+
+export function quantumShieldTrinity(wallet, codeSample = "") {
   const walletReport = walletRiskEngine(wallet);
-  const inventoryReport = cryptoInventoryEngine();
+  const inventoryReport = cryptoInventoryEngine(codeSample);
   const migrationReport = migrationShieldEngine();
+
+  const totalScore = Math.min(
+    100,
+    walletReport.score + inventoryReport.score
+  );
 
   return {
     platform: "Quantum Shield Trinity",
-    version: "0.1.0",
+    version: "0.2.0",
+    totalScore,
     walletReport,
     inventoryReport,
     migrationReport
