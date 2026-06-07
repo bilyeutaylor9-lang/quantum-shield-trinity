c3249507e0f4769a025060ce207db9fcbfdcb6e6
bilyeutaylor9-lang <bilyeutaylor9@gmail.com>
2026-06-07 15:58:23 -0700

    Add SARIF report generation to writeReports function

--- src/reporters/writeReports.js
+++ src/reporters/writeReports.js
@@ -1,33 +1,42 @@
-import fs from "fs";
+import fs from “fs”;
 
 export function writeReports({
-  htmlReport = "",
-  exportReport = {},
-  executiveReport = {}
+htmlReport = “”,
+exportReport = {},
+executiveReport = {},
+sarifReport = {}
 } = {}) {
-  fs.writeFileSync(
-    "report.html",
-    htmlReport,
-    "utf8"
-  );
+fs.writeFileSync(
+“report.html”,
+htmlReport,
+“utf8”
+);
 
-  fs.writeFileSync(
-    "report.json",
-    JSON.stringify(exportReport, null, 2),
-    "utf8"
-  );
+fs.writeFileSync(
+“report.json”,
+JSON.stringify(exportReport, null, 2),
+“utf8”
+);
 
-  fs.writeFileSync(
-    "executive-report.json",
-    JSON.stringify(executiveReport, null, 2),
-    "utf8"
-  );
+fs.writeFileSync(
+“executive-report.json”,
+JSON.stringify(executiveReport, null, 2),
+“utf8”
+);
 
-  return {
-    generatedFiles: [
-      "report.html",
-      "report.json",
-      "executive-report.json"
-    ]
-  };
+fs.writeFileSync(
+“report.sarif”,
+JSON.stringify(sarifReport, null, 2),
+“utf8”
+);
+
+return {
+generatedFiles: [
+“report.html”,
+“report.json”,
+“executive-report.json”,
+“report.sarif”
+],
+generatedAt: new Date().toISOString()
+};
 }
