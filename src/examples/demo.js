7be40a9455003ace32bf017bf763a2a752d4eeda
bilyeutaylor9-lang <bilyeutaylor9@gmail.com>
2026-06-07 09:45:11 -0700

    Update quantumShieldTrinity call with object parameters

--- src/examples/demo.js
+++ src/examples/demo.js
@@ -1,5 +1,10 @@
 import { quantumShieldTrinity } from "../index.js";
 
-const report = quantumShieldTrinity("0x123456789");
+const report = quantumShieldTrinity({
+  address: "0x123456789",
+  transactionCount: 22,
+  reusedAddress: true,
+  signedMessages: 5
+});
 
 console.log(JSON.stringify(report, null, 2));
